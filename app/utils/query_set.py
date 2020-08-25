7fafe4a38f7a1c58e93072060bc1cb3208bd4bca
Jorge Bastidas <jorgebastidas9@gmail.com>
2020-08-25 16:31:02 -0400

    optimized query set

--- app/utils/query_set.py
+++ app/utils/query_set.py
@@ -1,7 +1,6 @@
 from mongoengine import QuerySet, Document
 from bson.dbref import DBRef
 from bson.json_util import dumps
-import time
 
 class RefQuerySet(QuerySet):
 
@@ -9,9 +8,7 @@ class RefQuerySet(QuerySet):
         
         if not 'cities' in data:
             new_data = data._data
-            for key in new_data:
-                if isinstance(new_data[key], Document) or isinstance(new_data[key], DBRef):
-                    new_data[key] = str(new_data[key].id)
+            new_data = {key: str(new_data[key].id) if isinstance(new_data[key], Document) or isinstance(new_data[key], DBRef) else new_data[key] for key in new_data}
         else:
             new_data = {key: str(data[key].id) if isinstance(data[key], Document) or isinstance(data[key], DBRef) else data[key] for key in data}
                 
@@ -23,16 +20,18 @@ class RefQuerySet(QuerySet):
         
         data = self.set_related(data)
         
-        for key in data:
-            if isinstance(data[key], list):
-                data[key] = [self.set_related(old_data) for old_data in data[key]]
-                for list_data in data[key]:
-                    for key2 in list_data:
-                        if isinstance(list_data[key2], list):
-                            list_data[key2] = [self.set_related(old_data) for old_data in list_data[key2]]
+        data = {key: [self.set_related(old_data) for old_data in data[key]] if isinstance(data[key], list) else data[key] for key in data}
             
         return data
 
+    def get_relations(self,data):
+
+        data = self.get_related(data)
+
+        related_data = {key: [self.get_related(old_data) for old_data in data[key]] if isinstance(data[key], list) else data[key] for key in data}
+
+        return related_data
+
     def to_json(self):
-        data = [self.get_related(value) for value in self.select_related(max_depth=2)]
+        data = [self.get_relations(value) for value in self.select_related(max_depth=2)]
         return dumps(data)
