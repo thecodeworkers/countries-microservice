ced517ab62351919d08e725cd72276ec2fc09211
Jorge Bastidas <jorgebastidas9@gmail.com>
2020-08-26 08:18:56 -0400

    fix

--- app/utils/custom_tojson.py
+++ app/utils/custom_tojson.py
@@ -10,8 +10,4 @@ def to_json(data):
     data = {key: [parser_one_object(old_data) for old_data in data[key]] if isinstance(
         data[key], list) else data[key].id if isinstance(data[key], Document) else data[key] for key in data}
 
-    #mod_data = {key: data[key].id for key in data if isinstance(data[key], Document)}
-
-    # data.update(mod_data)
-
     return dumps(data)
