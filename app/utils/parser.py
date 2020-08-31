b6d3580ca4007f2444c27a864a72a0b6aa8ddb75
Liseth Gutierrez <liseth.yggdrasil@gmail.com>
2020-08-31 17:10:22 -0400

    get back to normal __iterate_object

--- app/utils/parser.py
+++ app/utils/parser.py
@@ -1,6 +1,5 @@
 from json import loads
 
-
 def parser_all_object(model):
     parser = __commonParser(model)
     parser = [__iterate_object(parse) for parse in parser]
@@ -18,21 +17,7 @@ def __commonParser(model):
     model_parser = loads(model_parser)
     return model_parser
 
-
 def __iterate_object(object):
-    object = __reference_object(object)
-    
-    if '_id' in object:
-        object['id'] = object['_id']
-        del object['_id']
-
-    return object
-
-def __reference_object(object):
-
-    for key in object:
-        if isinstance(object[key], dict):
-            if '$oid' in object[key]:
-                object[key] = object[key]['$oid']
-    
+    object['id'] = object['_id']['$oid']
+    del object['_id']
     return object
