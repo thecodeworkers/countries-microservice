0a05b6894d1fffe2aa8c9befe8bad3983db59f72
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-01-20 10:32:48 -0400

    Unique aplicado a countries

--- app/models/countries.py
+++ app/models/countries.py
@@ -4,7 +4,7 @@ from .states import States
 from bson.json_util import dumps
 
 class Countries(Document):
-    name = StringField(min_length=2, max_length=100, required=True)
+    name = StringField(min_length=2, max_length=100, required=True, unique=True)
     phone_prefix = StringField(min_length=2, max_length=10, required=True)
     active = BooleanField(required=True, default=1)
     states = ListField(ReferenceField(States, dbref=False))
