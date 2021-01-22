272613400fd8cd11bff8e404d967a16bcdf1f5ee
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-01-22 09:35:16 -0400

    added code

--- app/models/countries.py
+++ app/models/countries.py
@@ -1,10 +1,11 @@
-from mongoengine import Document, StringField, BooleanField, ListField, ReferenceField
+from mongoengine import Document, StringField, BooleanField, ListField, ReferenceField, IntField
 from ..utils import RefQuerySet, to_json
 from .states import States
 from bson.json_util import dumps
 
 class Countries(Document):
-    name = StringField(min_length=2, max_length=100, required=True, unique=True)
+    name = StringField(min_length=2, max_length=100, required=True)
+    code = IntField(min_value=1, required=True, unique=True)
     phone_prefix = StringField(min_length=2, max_length=10, required=True)
     active = BooleanField(required=True, default=1)
     states = ListField(ReferenceField(States, dbref=False))
